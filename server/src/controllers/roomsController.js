8bf42bad6372afc8834460d70f6947685c468a62
unknown <lucas46em@gmail.com>
2021-05-19 17:02:43 -0300

    day 1 go on

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,22 +1,53 @@
 import { constants } from "../util/constants.js"
 
 export default class RoomsController {
-    constructor(){ }
+    #user = new Map()
 
-    onNewConnection(socket){
+    constructor() {
+        this.rooms = new Map()
+     }
+
+    onNewConnection(socket) {
         const { id } = socket
-        console.log('connection stablished with',id)
+        console.log('connection stablished with', id)
+        this.#updateGlobalUserData(id,)
     }
 
-    joinRoom(socket,data){
-        console.log('dados recebidos',data)
-        socket.emit(constants.event.USER_CONNECTED,data)
+    joinRoom(socket, { user, room }) {
+        const userId = user.id = socket.id
+        const roomId = room.id
+
+        const updatedUserData = this.#updateGlobalUserData(userId, user, roomId)
+        
+        console.log({updatedUserData})
+    
+    
+    
+        socket.emit(constants.event.USER_CONNECTED, data)
     }
 
-    getEvents(){
+
+    #updateGlobalUserData(userId, userData = {}, roomId = '') { 
+        const user = this.#users.get(userId) ?? {}
+        const existingRoom = this.rooms.has(roomId)
+
+        const updatedUserData = new Attende({
+            ...user,
+            ...userData,
+            roomId,
+            //se for o unico na sala
+            isSpeaker: !existingRoom
+        })
+
+        this.#users.set(userId,updatedUserData)
+
+        return this.#users.get(userId)
+    }
+
+    getEvents() {
         const functions = Reflect.ownKeys(RoomsController.prototype)
             .filter(fn => fn !== 'constructor')
-            .map(name => [name,this[name].bind(this)])
+            .map(name => [name, this[name].bind(this)])
 
         return new Map(functions)
     }
